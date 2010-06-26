2034290d7295ad67f49cdb4be304894a74b6f4bb
Joe Daley <joe@fastmail.com.au>
2010-06-26 16:53:00 +1000

    Add HgVersionInfo unit tests.

--- MSBuildVersioning.Test/HgVersionInfoTest.cs
+++ MSBuildVersioning.Test/HgVersionInfoTest.cs
@@ -12,7 +12,34 @@ namespace MSBuildVersioning.Test
         public void GetRevisionNumberTest()
         {
             int revisionNumber = new HgVersionInfo().GetRevisionNumber();
-            Assert.Greater(revisionNumber, 0);
+            Assert.IsTrue(revisionNumber > 0);
+        }
+
+        [Test]
+        public void GetRevisionIdTest()
+        {
+            string revisionId = new HgVersionInfo().GetRevisionId();
+            Assert.AreEqual(12, revisionId.Length);
+        }
+
+        [Test]
+        public void IsWorkingCopyDirtyTest()
+        {
+            new HgVersionInfo().IsWorkingCopyDirty();
+        }
+
+        [Test]
+        public void GetBranchTest()
+        {
+            string branch = new HgVersionInfo().GetBranch();
+            Assert.IsTrue(branch.Length > 0);
+        }
+
+        [Test]
+        public void GetTagsTest()
+        {
+            string tags = new HgVersionInfo().GetTags();
+            Assert.IsNotNull(tags);
         }
     }
 }

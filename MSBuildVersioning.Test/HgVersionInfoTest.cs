da7ac70f9eda8c42a304ce9dd2923bcb6f4d16a2
Joe Daley <joe@fastmail.com.au>
2010-06-27 20:11:18 +1000

    Create separate test repositories for HgVersionInfoTest.

--- MSBuildVersioning.Test/HgVersionInfoTest.cs
+++ MSBuildVersioning.Test/HgVersionInfoTest.cs
@@ -8,38 +8,49 @@ namespace MSBuildVersioning.Test
     [TestFixture]
     public class HgVersionInfoTest
     {
+        public HgVersionInfo Hg1
+        {
+            get { return new HgVersionInfo() { Path = @"..\..\..\TestRepositories\Hg1" }; }
+        }
+
+        public HgVersionInfo Hg2
+        {
+            get { return new HgVersionInfo() { Path = @"..\..\..\TestRepositories\Hg2" }; }
+        }
+
         [Test]
         public void GetRevisionNumberTest()
         {
-            int revisionNumber = new HgVersionInfo().GetRevisionNumber();
-            Assert.IsTrue(revisionNumber > 0);
+            Assert.AreEqual(2, Hg1.GetRevisionNumber());
+            Assert.AreEqual(4, Hg2.GetRevisionNumber());
         }
 
         [Test]
         public void GetRevisionIdTest()
         {
-            string revisionId = new HgVersionInfo().GetRevisionId();
-            Assert.AreEqual(12, revisionId.Length);
+            Assert.AreEqual("1024d08c6b37", Hg1.GetRevisionId());
+            Assert.AreEqual("bf82f571c792", Hg2.GetRevisionId());
         }
 
         [Test]
         public void IsWorkingCopyDirtyTest()
         {
-            new HgVersionInfo().IsWorkingCopyDirty();
+            Assert.IsFalse(Hg1.IsWorkingCopyDirty());
+            Assert.IsTrue(Hg2.IsWorkingCopyDirty());
         }
 
         [Test]
         public void GetBranchTest()
         {
-            string branch = new HgVersionInfo().GetBranch();
-            Assert.IsTrue(branch.Length > 0);
+            Assert.AreEqual("default", Hg1.GetBranch());
+            Assert.AreEqual("formal", Hg2.GetBranch());
         }
 
         [Test]
         public void GetTagsTest()
         {
-            string tags = new HgVersionInfo().GetTags();
-            Assert.IsNotNull(tags);
+            Assert.AreEqual("", Hg1.GetTags());
+            Assert.AreEqual("formal-1.0", Hg2.GetTags());
         }
     }
 }

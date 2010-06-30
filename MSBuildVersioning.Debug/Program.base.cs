2e7f5d6d6faf8466e321eb3a47781dcb11ad79f3
Joe Daley <joe@fastmail.com.au>
2010-06-30 20:09:45 +1000

    Add template Program.cs to Debug project.

---
+++ MSBuildVersioning.Debug/Program.base.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MSBuildVersioning.Test;
+
+namespace MSBuildVersioning.Debug
+{
+    /*
+     * To debug the MSBuildVersioning code, you need to either:
+     * (a) attach the Visual Studio debugger to NUnit (requires Visual Studio Professional); or
+     * (b) call the desired code from here, and then run this project in the Visual Studio debugger
+     * 
+     * This is a template file. Copy this file, rename it to Program.cs, and then modify it as
+     * desired.
+     */
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            new HgVersionInfoTest().GetRevisionIdTest();
+        }
+    }
+}
